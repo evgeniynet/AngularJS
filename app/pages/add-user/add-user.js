eecc74b9ca9a15815d1be543e76e0b929af22237
Nastya <nastya_01.88@mail.ru>
2016-02-18 11:23:29 +0200

    fix

---
+++ app/pages/add-user/add-user.js
@@ -0,0 +1,16 @@
+import {Page, NavController} from 'ionic/ionic';
+
+/*
+  Generated class for the AddUserPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Page({
+  templateUrl: 'build/pages/add-user/add-user.html',
+})
+export class AddUserPage {
+  constructor(nav: NavController) {
+    this.nav = nav;
+  }
+}
