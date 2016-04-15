65899c00f5043aeb90d0e5b31e158d168f829523
Eugene <jtrue@mail.ru>
2016-04-15 14:36:12 +0300

    fixed style error

--- app/app.ts
+++ app/app.ts
@@ -78,12 +78,12 @@ class MyApp {
         
         // set first pages
         //this.rootPage = HelloIonicPage; return;
-        this.rootPage = TicketsPage; return;
+        //this.rootPage = TicketsPage; return;
         //this.rootPage = ExpensesPage; return;
         //this.rootPage = ExpenseCreatePage; return;
         //this.rootPage = TimelogsPage; return;
         //this.rootPage = TimelogPage; return;
-        //this.rootPage = AccountsPage; return;
+        this.rootPage = AccountsPage; return;
         //this.rootPage = TicketCreatePage; return;
       
         if (config.current.user.is_techoradmin)
