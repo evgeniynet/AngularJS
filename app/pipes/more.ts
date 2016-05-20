46b032192981309849e483d95511fa49f0dfa0ff
Eugene <jtrue@mail.ru>
2016-05-20 13:56:33 +0300

    fix pipe

--- app/pipes/more.ts
+++ app/pipes/more.ts
@@ -5,10 +5,10 @@ import {Pipe} from '@angular/core';
 })
 export class MorePipe {
     transform(value, args) {
-        args = args || [[100, "VV"]]; 
+        args = args || [100, "VV"]; 
         value = value || 0;
-        let max = args[0][0]; 
-        let template = args[0][1] || "VV"; 
+        let max = args[0]; 
+        let template = args[1] || "VV"; 
         if (value >= max)
         value = (max-1) + "<sup>+</sup>";
         value = template.replace("VV", value.toString());
