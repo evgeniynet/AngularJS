f8d8cd6f896ac371d80061cd323f281014f6fbdf
Nastya <nastya_01.88@mail.ru>
2016-10-03 09:35:59 +0300

    fix pages

--- app/directives/helpers.ts
+++ app/directives/helpers.ts
@@ -160,7 +160,7 @@ export const FileUrlHelper = {
                 img = "<img class=\"attachment\" src=\"" + file + "\">";
             else
                 img = "<ion-icon name=\"md-document\" role=img dark class=\"button_circle ion-md-document\" aria-label=\"md-document\"></ion-icon>&nbsp;" + (name ||  decodeURIComponent(file.split("/").slice(-1))) + "<p></p>";
-            return "<p/><a class=\"comment_image_link\"" +
+            return "<p/><a class=\"comment_image_link files-tkts\"" +
                 (FileUrlHelper.isPhonegap() ? (" href=# onclick='window.open(\"" + file + "\", \"_blank\", \"location=no,EnableViewPortScale=yes\")'>" + img + "</a>") :
                  (" target=\"_blank\" href=\"" +file + "\">"+img + "</a>"));
         }
