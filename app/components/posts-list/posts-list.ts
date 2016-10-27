9e5317e837022c214478aa7d8a7716368eeb12ba
Eugene <jtrue@mail.ru>
2016-10-27 19:49:18 +0300

    added closed and reopened

--- app/components/posts-list/posts-list.ts
+++ app/components/posts-list/posts-list.ts
@@ -20,16 +20,21 @@ export class PostsListComponent {
     constructor() {
     }  
 
+    filter()
+    {
+        this._posts = this.is_showlogs ? this.posts : this.posts.filter(item => !!~["Initial Post", "Response", "Closed", "ReOpened"].indexOf(item.log_type));
+    }
+
     ngOnInit() {
-    this._posts = this.is_showlogs ? this.posts : this.posts.filter(item => !!~["Initial Post", "Response"].indexOf(item.log_type));
+      this.filter();
     }
 
     ngOnChanges(event) { 
         if ("posts" in event || "is_showlogs" in event) {
              if (event.posts.isFirstChange() && event.posts.currentValue !== null)
                  return;
-             this._posts = this.is_showlogs ? this.posts : this.posts.filter(item => !!~["Initial Post", "Response"].indexOf(item.log_type));
-         }
+             this.filter();
+          }
     }
 
     /*get posts()
