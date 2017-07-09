6cb29584e1298d4b281fcfbcff0aa624f42f8d81
Eugene <jtrue@mail.ru>
2017-07-09 15:06:55 +0300

    added checks

--- app/pages/invoice-details/invoice-details.ts
+++ app/pages/invoice-details/invoice-details.ts
@@ -7,7 +7,7 @@ import {InvoicesPage} from '../invoices/invoices';
 import {InfinitySelectModal} from '../modals/modals';
 
 @Page({
-  templateUrl: 'build/pages/invoice-details/invoice-details.html',
+    templateUrl: 'build/pages/invoice-details/invoice-details.html',
     pipes: [GravatarPipe],
 })
 export class InvoiceDetailsPage {
@@ -21,14 +21,14 @@ export class InvoiceDetailsPage {
     onPageLoaded() {
         this.invoice = this.navParams.data || {};
         if (this.invoice.id)
-        this.title = `Send Invoice #${this.invoice.id} to\u00a0${this.invoice.account_name}`;
+            this.title = `Send Invoice #${this.invoice.id} to\u00a0${this.invoice.account_name}`;
         else 
             this.title = `Create Invoice on\u00a0${this.invoice.account_name}`;
 
         //this.invoice.account = { id: this.params.account_id || 0, name: this.params.account_name || this.config.getCurrent("user").account_name };
         this.dataProvider.getInvoice(this.invoice.id, this.invoice.account_id, this.invoice.project_id).subscribe(
             data => {
-              
+                
                 if (data.length == 1)
                     data = data[0];
                 if (data.recipients)
@@ -37,39 +37,47 @@ export class InvoiceDetailsPage {
                     return a.is_accounting_contact < b.is_accounting_contact ? 1 : -1;
                 });
                 this.invoice = data;
-                    },
+            },
             error => {
                 console.log(error || 'Server error');
             }
-        );
-  }
-   addRecipientsButton() {
-    console.log("Function connect");
-    let myModal = Modal.create(InfinitySelectModal,{
-                            hidden :false,
-                            items: [],
-                            name:"User",
-                            selected:9032,
-                            url:"users",
-                            value:"1NN Test "});
+            );
+    }
+    addRecipientsButton() {
+        let myModal = Modal.create(InfinitySelectModal,{
+            hidden :false,
+            items: [],
+            name:"User",
+            selected:9032,
+            url:"users",
+            value:"1NN Test "});
+        
         myModal.onDismiss(data1 => {
-            console.log(data1);
-            var RegN= /\(/;
-            var RegM= /\)/;
-            var N=data1.name.search(RegN);
-            var M=data1.name.search(RegM);
-            data1.fullname=data1.name.substring(0,N);
-            data1.email=data1.name.substring(N+1,M);
+            if (!data1.name)
+                return;
+            var regExp = /.*\(([^)]+)\)/;
+            var matches = regExp.exec(data1.name);
+            if (matches.length > 2)
+            {
+                data1.fullname=matches[1];
+                data1.email=matches[2];
+            }
+            else
+            {
+                data1.fullname=data1.name;
+                data1.email=data1.name;
+            }
             data1.is_accounting_contact=true;
-            //console.log(data1.fullname, data1.email);
-            this.invoice.recipients.unshift (data1);
-            
-            
+            this.invoice.recipients = this.invoice.recipients || [];
+            if (!this.invoice.recipients.filter(v => v.email == data1.email).length)
+            {
+                this.invoice.recipients.unshift (data1);
+            }
         });
         this.nav.present(myModal);
-            }
-  onPageWillEnter() {
-            this.view.setBackButtonText('');
+    }
+    onPageWillEnter() {
+        this.view.setBackButtonText('');
     }
 
     setDate(date, showmonth?, istime?) {
@@ -90,9 +98,9 @@ export class InvoiceDetailsPage {
             return;
         }
         //proof double click
-            if (this.invoice.in_progress && Date.now() - this.invoice.in_progress < 1500) {return;}
-            this.invoice.in_progress = Date.now();
-            
+        if (this.invoice.in_progress && Date.now() - this.invoice.in_progress < 1500) {return;}
+        this.invoice.in_progress = Date.now();
+        
         var emails = "";
         this.invoice.recipients.forEach((v) => {
             if (v.is_accounting_contact) { emails += v.email + ","; }
@@ -113,16 +121,16 @@ export class InvoiceDetailsPage {
         data.recipients = emails;
 
         this.apiData.get('invoices/' + (this.invoice.id || ""), data, !this.invoice.id ? 'POST' : 'PUT').subscribe(
-                data => {
-                    this.nav.alert('Hurray! Invoice sent :)');
-                    if (!this.invoice.id)
-                        this.nav.popTo(this.nav.getByIndex(this.nav.length() - 3));
-                    else
-                        this.nav.pop();
-                },
-                error => {
-                    console.log(error || 'Server error');
-                }
+            data => {
+                this.nav.alert('Hurray! Invoice sent :)');
+                if (!this.invoice.id)
+                    this.nav.popTo(this.nav.getByIndex(this.nav.length() - 3));
+                else
+                    this.nav.pop();
+            },
+            error => {
+                console.log(error || 'Server error');
+            }
             );
     }
 }
