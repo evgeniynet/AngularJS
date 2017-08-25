bd788afeda6030103254f225757cbfd7c2d551c5
dimad-git <0684509627@ukr.net>
2017-08-25 15:35:32 +0300

    add search in location-detail

--- app/pages/location-details/location-details.ts
+++ app/pages/location-details/location-details.ts
@@ -1,9 +1,12 @@
 import {Page, Config, Nav, NavParams, ViewController, Modal} from 'ionic-angular';
 import {DataProvider} from '../../providers/data-provider';
-import {TicketProvider} from '../../providers/providers';
 import {TicketCreatePage} from '../modals/modals'; 
 import {TicketDetailsPage} from '../ticket-details/ticket-details';
 import {TicketsListComponent} from '../../components/tickets-list/tickets-list';
+import {ApiData, TicketProvider} from '../../providers/providers';
+import {addp} from '../../directives/helpers';
+import {AjaxSearchPage} from '../ajax-search/ajax-search';
+
 
 @Page({
   templateUrl: 'build/pages/location-details/location-details.html',
@@ -15,8 +18,12 @@ export class LocationDetailsPage {
   pages: Array<any>;
   tabsTicket: string; 
   is_ready: boolean = false;
+  search_results: any;
+  test: boolean;
+  term: string = '';
+  busy: boolean;
 
-  constructor(private nav: Nav, private navParams: NavParams, private dataProvider: DataProvider, private ticketProvider: TicketProvider, private config: Config, private view: ViewController) {
+  constructor(private nav: Nav, private navParams: NavParams, private apiData: ApiData, private dataProvider: DataProvider, private ticketProvider: TicketProvider, private config: Config, private view: ViewController) {
   }
 
   onPageLoaded()
@@ -60,4 +67,84 @@ export class LocationDetailsPage {
       }, 500);
     }
   }
+   searchItems(searchbar) {
+        // Reset items back to all of the items
+        this.search_results = [];
+
+        // set q to the value of the searchbar
+        var q = searchbar.value;
+
+        // if the value is an empty string don't filter the search_results
+        if (q.trim() == '' || this.busy) {
+            return;
+        }
+
+        if (q.length > 1)
+        {
+            var timer = setTimeout(() => { this.busy = true; }, 500);
+            this.searchItemsAPI(q, timer);
+        }
+    }
+
+     searchItemsAPI(term, timer) {
+        this.search_results = [];
+        let url = "tickets?query=all"; //status=allopen&
+        let pager = { limit: 3 };
+        let is_ticket = term[term.length - 1] == " " || term[term.length - 1] == ",";
+        if (!is_ticket) term += "*";
+        else url = "tickets/" + term.trim() + ",";
+        this.apiData.getPaged(addp(url, "search", term), pager).subscribe(
+            data => {
+                if (timer) {
+                    clearTimeout(timer);
+                    this.busy = false;
+                }
+                this.search_results = data;
+            },
+            error => {
+                if (timer) {
+                    clearTimeout(timer);
+                    this.busy = false;
+                }
+                console.log(error || 'Server error');
+            }
+            );
+    }
+
+
+    clearSearch(searchbar?)
+    {
+        this.search_results = [];
+        this.busy = false;
+        if (searchbar) searchbar.value = "";
+    }
+
+    getSearch(searchbar) {
+        this.test = false;
+        this.clearSearch();
+        // Reset items back to all of the items
+        // set q to the value of the searchbar
+        let term = searchbar.target.value;
+        if (term.length < 4)
+            term += "    ";
+        let list = { search: term };
+        this.test = false;
+        this.nav.push(AjaxSearchPage, list);
+    }
+
+        ToTicket(data)
+    {
+        this.test = false;
+        this.clearSearch();
+        if (data)
+        {
+            this.ticketProvider.getTicketsList("", "", "",{ "limit": 25 });
+            setTimeout(() => {
+                this.nav.push(TicketDetailsPage, data);
+            //if (!this.counts[this.ticket_tab])
+            //    this.counts = {};
+            //this.counts[this.ticket_tab] = (this.ticketProvider._dataStore[this.ticket_tab] || {}).length++;
+            }, 500);
+        }
+    }
 }
