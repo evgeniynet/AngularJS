e7c70c0551fd860f165e69e0af6a48601ac257f1
dimad-git <0684509627@ukr.net>
2017-08-25 15:25:49 +0300

    add serch in locations page

--- app/pages/locations/locations.ts
+++ app/pages/locations/locations.ts
@@ -1,6 +1,10 @@
 import {Page, Config, Nav} from 'ionic-angular';
 import {DataProvider} from '../../providers/data-provider';
 import {LocationsListComponent, ActionButtonComponent} from '../../components/components';
+import {AjaxSearchPage} from '../ajax-search/ajax-search';
+import {ApiData, TicketProvider} from '../../providers/providers';
+import {addp} from '../../directives/helpers';
+import {TicketDetailsPage} from '../ticket-details/ticket-details';
 
 
 @Page({
@@ -16,8 +20,11 @@ export class LocationsPage {
     pager: any;
     locations: Array<any>;
     LIMIT: number = 500;
+    search_results: any;
+    test: boolean;
+    term: string = '';
 
-    constructor(private nav: Nav, private config: Config, private dataProvider: DataProvider) {
+    constructor(private nav: Nav, private config: Config, private dataProvider: DataProvider, private apiData: ApiData, private ticketProvider: TicketProvider) {
   }
     
     onPageLoaded()
@@ -72,5 +79,85 @@ export class LocationsPage {
         }
         this.pager.page += 1;
         this.getItems(infiniteScroll, null);
+    }
+     searchItems(searchbar) {
+        // Reset items back to all of the items
+        this.search_results = [];
+
+        // set q to the value of the searchbar
+        var q = searchbar.value;
+
+        // if the value is an empty string don't filter the search_results
+        if (q.trim() == '' || this.busy) {
+            return;
+        }
+
+        if (q.length > 1)
+        {
+            var timer = setTimeout(() => { this.busy = true; }, 500);
+            this.searchItemsAPI(q, timer);
+        }
+    }
+
+     searchItemsAPI(term, timer) {
+        this.search_results = [];
+        let url = "tickets?query=all"; //status=allopen&
+        let pager = { limit: 3 };
+        let is_ticket = term[term.length - 1] == " " || term[term.length - 1] == ",";
+        if (!is_ticket) term += "*";
+        else url = "tickets/" + term.trim() + ",";
+        this.apiData.getPaged(addp(url, "search", term), pager).subscribe(
+            data => {
+                if (timer) {
+                    clearTimeout(timer);
+                    this.busy = false;
+                }
+                this.search_results = data;
+            },
+            error => {
+                if (timer) {
+                    clearTimeout(timer);
+                    this.busy = false;
+                }
+                console.log(error || 'Server error');
+            }
+            );
+    }
+
+
+    clearSearch(searchbar?)
+    {
+        this.search_results = [];
+        this.busy = false;
+        if (searchbar) searchbar.value = "";
+    }
+
+    getSearch(searchbar) {
+        this.test = false;
+        this.clearSearch();
+        // Reset items back to all of the items
+        // set q to the value of the searchbar
+        let term = searchbar.target.value;
+        if (term.length < 4)
+            term += "    ";
+        let list = { search: term };
+        this.test = false;
+        this.nav.push(AjaxSearchPage, list);
+    }
+
+        gotoTicket(data)
+    {
+        this.test = false;
+        this.clearSearch();
+        if (data)
+        {
+            this.ticketProvider.getTicketsList("", "", "",{ "limit": 25 });
+            setTimeout(() => {
+                this.nav.push(TicketDetailsPage, data);
+            //if (!this.counts[this.ticket_tab])
+            //    this.counts = {};
+            //this.counts[this.ticket_tab] = (this.ticketProvider._dataStore[this.ticket_tab] || {}).length++;
+            }, 500);
+        }
     }
 }
