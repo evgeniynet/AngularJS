3939c7b51e373fbb3df1cb2fd264add0fb041cc6
Eugene <jtrue@mail.ru>
2018-05-17 18:28:50 +0300

    disabled focuser

--- app/directives/focuser.ts
+++ app/directives/focuser.ts
@@ -6,6 +6,8 @@ export class Focuser {
 	constructor(public renderer: Renderer, public elementRef: ElementRef) { }
 
 	ngOnInit() {
+		//disabled focuser
+		return;
 		//search bar is wrapped with a div so we get the child input
 		const searchInput = this.elementRef.nativeElement.querySelector('input');
 		setTimeout(() => {
