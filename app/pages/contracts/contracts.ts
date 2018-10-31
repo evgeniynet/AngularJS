0fcf7974b745e71a451eb0af8b3913c5dc7b2ea9
dima <0684509627@ukr.net>
2018-10-31 21:30:37 +0200

    Add tables Tkt#10863
    Contract and PrePaid Packs

--- app/pages/contracts/contracts.ts
+++ app/pages/contracts/contracts.ts
@@ -11,7 +11,8 @@ export class ContractsPage {
     LIMIT: number = 15;
     count: number;
     account: Object;
-    is_empty: boolean;
+    is_empty_contracts: boolean;
+    is_empty_prepaid: boolean;
     busy: boolean;
     params: any;
     pager: any;
@@ -21,7 +22,8 @@ export class ContractsPage {
 
 
     constructor(private nav: Nav, private dataProvider: DataProvider, private config: Config, private navParams: NavParams, private view: ViewController) {
-        this.is_empty = false;
+        this.is_empty_contracts = false;
+        this.is_empty_prepaid = false;
     }
     
     onPageLoaded() {
@@ -46,7 +48,9 @@ onPageWillEnter()
         this.getItems(null, timer);
     }
     else
-        this.is_empty = true;
+        this.is_empty_prepaid = true;
+        this.is_empty_contracts = true;
+
     }
 
 
@@ -54,10 +58,10 @@ onPageWillEnter()
         this.dataProvider.getContracts(this.pager).subscribe(
             data => {
                 if (timer) {
+                    this.is_empty_contracts = !data.length;
                     clearTimeout(timer);
                     this.busy = false;
                     this.contracts = data;
-                    console.log(this.contracts,"contracts");
                 }
                 else
                     this.contracts.push(...data);
@@ -79,6 +83,7 @@ onPageWillEnter()
         this.dataProvider.getPrepaid_packs(this.params.account.id, this.pager).subscribe(
             data => {
                 if (timer) {
+                    this.is_empty_prepaid = !data.length;
                     clearTimeout(timer);
                     this.busy = false;
                     this.prepaid_packs = data;
@@ -102,7 +107,7 @@ onPageWillEnter()
     }
 
     doInfinite(infiniteScroll) {
-        if (this.is_empty || this.count < this.LIMIT) {
+        if (this.is_empty_contracts || this.count < this.LIMIT) {
             infiniteScroll.enable(false);
             infiniteScroll.complete();
             return;
@@ -111,17 +116,6 @@ onPageWillEnter()
         this.getItems(infiniteScroll, null);
     }
 
-   
-    
-    setDate(date, showmonth?, istime?) {
-        if (date){
-        var time_offset = this.config.getCurrent("timezone_offset");     
-        date = new Date(date.substring(0,23)+"Z");
-        date = new Date(date.setTime(date.getTime() + time_offset*60*60*1000)).toJSON();
-        return getDateTime(date, showmonth, istime);
-    }
-     return null;
-    }
 
     getCurrency(value) {
         return getCurrency(value);
