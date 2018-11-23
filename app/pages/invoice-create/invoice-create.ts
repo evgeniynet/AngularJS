2b86124aac93ad3a9191e426f3c63fe214ad7548
Eugene <jtrue@mail.ru>
2018-11-23 15:01:41 +0200

    removed old code

--- app/pages/invoice-create/invoice-create.ts
+++ app/pages/invoice-create/invoice-create.ts
@@ -21,7 +21,6 @@ export class InvoiceCreatePage {
     inc : number;
     isbillable: boolean;
     contract: any = [];
-    contract_id: any;
     timecount: any;
     timelogs: any = [];
     pager: any = [];
@@ -29,20 +28,14 @@ export class InvoiceCreatePage {
     recipients: any = [];
     timecount_nonwork: any;
     mintime: number;
-    time: any = {};
+    data: any = {};
     timee: any = {};
     timenote: string;
     title: string = "";
     he: any;
     selects: any = {};
     displayFormat: string;
-    minuteValues: Array<number> = [0, 15, 30, 45, 0];
-    start_time: string = "";
-    stop_time: string = "";
     UserDateOffset: number = -5;
-    //start_stop_hours: number = 0;
-    //@ViewChild('starttime') starttime:DateTime;
-    //@ViewChild('stoptime') stoptime:DateTime;
 
     constructor(private nav: Nav, private navParams: NavParams, private dataProvider: DataProvider, private timeProvider: TimeProvider, private config: Config, private view: ViewController) {
     this.timelogs = [{"time_id":154101,"project_name":"","user_name":"Eugene Tolmachov","user_email":"eugene@micajah.com","user_id":496558,"note":"","date":"2018-11-14T11:09:00.0000000","stop_time":null,"start_time":null,"hours":0.2500,"fb_id":0,"is_project_log":true,"ticket_id":0,"task_type_id":51873,"task_type":"Onsite - Residential","project_id":0,"account_id":-1,"ticket_number":0,"account_name":"Demo Account Testing","ticket_subject":"","invoice_id":0,"no_invoice":false,"invoice_pseudo_id":"      ","qb_id":0,"payment_id":0,"prepaid_pack_name":"","prepaid_pack_id":0,"user_profile_image":"https://secure.gravatar.com/avatar/8a3bd35c5518ff857b460afd1ddb629d?d=identicon&r=g&s=40","hidden_from_invoice":false,"time_offset":2,"created_user_name":"Eugene Tolmachov","created_user_id":496558,"created_time":"2018-11-14T13:09:00.0000000","updated_user_name":"","updated_user_id":0,"updated_time":null,"non_working_hours":0.0000,"contract_name":"Main","contract_id":81}, {"time_id":154101,"project_name":"","user_name":"Eugene Tolmachov","user_email":"eugene@micajah.com","user_id":496558,"note":"","date":"2018-11-14T11:09:00.0000000","stop_time":null,"start_time":null,"hours":0.2500,"fb_id":0,"is_project_log":true,"ticket_id":0,"task_type_id":51873,"task_type":"Onsite - Residential","project_id":0,"account_id":-1,"ticket_number":0,"account_name":"Demo Account Testing","ticket_subject":"","invoice_id":0,"no_invoice":false,"invoice_pseudo_id":"      ","qb_id":0,"payment_id":0,"prepaid_pack_name":"","prepaid_pack_id":0,"user_profile_image":"https://secure.gravatar.com/avatar/8a3bd35c5518ff857b460afd1ddb629d?d=identicon&r=g&s=40","hidden_from_invoice":false,"time_offset":2,"created_user_name":"Eugene Tolmachov","created_user_id":496558,"created_time":"2018-11-14T13:09:00.0000000","updated_user_name":"","updated_user_id":0,"updated_time":null,"non_working_hours":0.0000,"contract_name":"Main","contract_id":81}];
@@ -51,108 +44,27 @@ export class InvoiceCreatePage {
     console.log(this.timelogs);
     }
 
-    decrement()
-    {
-        this.timecount = Math.max(Number(this.timecount) - this.inc, 0).toFixed(2);
-    }
-
-    increment()
-    {
-        this.timecount = (Number(this.timecount) + this.inc).toFixed(2);
-    }
-
-    decrement_nonwork()
-    {
-        this.timecount_nonwork = Math.max(Number(this.timecount_nonwork) - this.inc, 0).toFixed(2);
-    }
-
-    increment_nonwork()
-    {
-        this.timecount_nonwork = (Number(this.timecount_nonwork) + this.inc).toFixed(2);
-    }
-
-    ngAfterViewInit() {
-      //console.log(this.config.current.recent);
-      //this.starttime.displayFormat = this.displayFormat;
-//console.log(this.starttime.displayFormat);
-
-}
-
-AddHours(date, hours)
-{
-    if (date){
-        date = new Date(date.substring(0,19)+"Z");
-        date = new Date(date.setTime(date.getTime() + hours*60*60*1000)).toJSON();
-        return date;
-    }
-    return date;
-}
 
 ngOnInit()
 {    
     this.UserDateOffset = this.config.getCurrent("timezone_offset");
-    this.time = this.navParams.data || {};
+    this.data = this.navParams.data || {};
     this.pager = { page: 0 };
-    
-    let name = (this.time.user_name + " " + this.time.user_email).trim().split(' ')[0];
-            if (this.time.time_id)
-            {
-                this.title = `Timelog #${this.time.time_id} by\u00a0${name} on\u00a0` + this.setDate(this.time.created_time, false, true);
-                //fix timezone
-                this.start_time = this.AddHours(this.time.start_time, this.time.time_offset);
-                this.stop_time = this.AddHours(this.time.stop_time, this.time.time_offset);
-                //if (this.start_time && this.stop_time)
-                //    this.start_stop_hours = Number(Math.round((+(new Date(this.stop_time)) - +(new Date(this.start_time))) / 60000)/60);
-            }
-            else if (this.time.number)
-                this.title = `#${this.time.number} ${this.time.subject}`;
-            else
-                this.title = "Add Invoice";
-            if (this.time.invoice_id > 0)
-            {
-                this.title = `Invoiced #${this.time.invoice_id} on\u00a0` + this.setDate(this.AddHours(this.time.date, this.time.time_offset), false, true);
-            }
 
-            this.mintime = this.config.getCurrent("time_minimum_time") || 0.25;
-            this.mintime = this.mintime > 0 ? this.mintime : 0.25;
-
-            this.isbillable = this.time.no_invoice;
-
-            this.inc = this.config.getCurrent("time_hour_increment") > 0 ? this.config.getCurrent("time_hour_increment") : 0.25;
-
-            this.displayFormat = getPickerDateTimeFormat(false, true);
-
-            if (this.inc >= 1)
-                this.minuteValues = [0];   
-            else if (this.inc != 0.25)
-            { 
-                this.minuteValues = [];
-                let min = 0;
-                //do { this.minuteValues.push(min); min += 60*this.inc;}
-                do { this.minuteValues.push(min); min += 5;}
-                while (min < 60);
-                this.minuteValues.push(0);
-            }
-
-
-            this.timecount = (this.time.hours || this.mintime).toFixed(2);
-            this.timecount_nonwork = (this.time.non_working_hours || 0).toFixed(2);
-            if (this.timecount_nonwork<0) 
-                this.timecount_nonwork = 0;
-            this.timenote = linebreaks(this.time.note || "", true);
             this.he = this.config.getCurrent("user");
 
             let recent : any = {};
 
-            if (!this.time.number && !this.time.time_id && !(this.time.account || {}).id)
+            if (!this.data.number && !(this.data.account || {}).id)
             {
                 recent = this.config.current.recent || {};
             }
 
-            let account_id = (this.time.account || {}).id || this.time.account_id || (recent.account || {}).selected || this.he.account_id || -1;
-            this.contract_id = (this.time.contract || {}).id || this.time.contract_id || (recent.contract || {}).selected || 0;
-            let project_id = (this.time.project || {}).id || this.time.project_id || (recent.project || {}).selected || 0;
-            this.getContract(this.contract_id);
+            let account_id = (this.data.account || {}).id || this.data.account_id || (recent.account || {}).selected || this.he.account_id || -1;
+            let contract_id = (this.data.contract || {}).id || this.data.contract_id || (recent.contract || {}).selected || 0;
+            let project_id = (this.data.project || {}).id || this.data.project_id || (recent.project || {}).selected || 0;
+            if(contract_id)
+                this.getContract(contract_id);
 
             this.selects = {
                     "user" : {
@@ -171,44 +83,43 @@ ngOnInit()
                 },
                 "account" : {
                     name: "Account", 
-                    value:  (this.time.account || {}).name || this.time.account_name || (recent.account || {}).value || this.he.account_name,
+                    value:  (this.data.account || {}).name || this.data.account_name || (recent.account || {}).value || this.he.account_name,
                     selected: account_id,
                     url: "accounts?is_with_statistics=false",
                     hidden: false,
-                    is_disabled: this.time.ticket_number
+                    is_disabled: this.data.ticket_number
                 },
                 "project" : {
                     name: "Project", 
-                    value:  this.time.project_name || (recent.project || {}).value || "Default",
+                    value:  this.data.project_name || (recent.project || {}).value || "Default",
                     selected: project_id,
                     url: `projects?account=${account_id}&is_with_statistics=false`,
                     hidden: false,
-                    is_disabled: this.time.ticket_number
+                    is_disabled: this.data.ticket_number
                 },
                 "ticket" : {
                     name: "Ticket", 
-                    value: this.time.ticket_number ? `#${this.time.ticket_number}: ${this.time.ticket_subject}` : "Choose (optional)",
-                    selected: this.time.ticket_number || 0,
+                    value: this.data.ticket_number ? `#${this.data.ticket_number}: ${this.data.ticket_subject}` : "Choose (optional)",
+                    selected: this.data.ticket_number || 0,
                     url: `tickets?status=open&account=${account_id}&project=${project_id}`,
-                    hidden: this.time.is_project_log || false,
-                    is_disabled: this.time.task_type_id
+                    hidden: this.data.is_project_log || false,
+                    is_disabled: this.data.task_type_id
                 },
                  "contract" : { 
                     name: "Contract", 
-                    value: this.time.contract_name || (recent.contract || {}).value || "Choose",
-                    selected: this.time.contract_id || this.config.getRecent("contract").selected || 0,
+                    value: this.data.contract_name || (recent.contract || {}).value || "Choose",
+                    selected: this.data.contract_id || this.config.getRecent("contract").selected || 0,
                     url: `contracts?account_id=${account_id}`,
                     hidden: false
                 },
                 "prepaidpack" : {
                     name: "PrePaid Pack", 
-                    value: this.time.prepaid_pack_name || (recent.prepaidpack || {}).value || "Choose",
-                    selected: this.time.prepaid_pack_id || this.config.getRecent("prepaidpack").selected || 0,
-                    url: `prepaid_packs?contract_id=${this.contract_id}`,
+                    value: this.data.prepaid_pack_name || (recent.prepaidpack || {}).value || "Choose",
+                    selected: this.data.prepaid_pack_id || this.config.getRecent("prepaidpack").selected || 0,
+                    url: `prepaid_packs?contract_id=${contract_id}`,
                     hidden: false
                 }
             };
-            this.contract_id = this.selects.contract.selected;
         }
 
         saveSelect(event){
@@ -217,8 +128,6 @@ ngOnInit()
             let ticket_id = this.selects.ticket.selected;
             let project_id = this.selects.project.selected;
             let contract_id = this.selects.contract.selected;
-               if (contract_id) 
-                   this.getContract(contract_id);
             let prepaidpack_id = this.selects.prepaidpack.selected;    
         //change url on related lists
         switch (name) {
@@ -238,8 +147,8 @@ ngOnInit()
             this.selects.prepaidpack.value = "Choose (optional)";
             this.selects.prepaidpack.selected = 0;
             account_id = event.id;
-            this.selects.ticket.hidden = this.time.is_project_log || this.time.task_type_id || false;
-            if (!this.time.task_type_id){
+            this.selects.ticket.hidden = this.data.is_project_log || this.data.task_type_id || false;
+            if (!this.data.task_type_id){
                 this.selects.ticket.url = `tickets?status=open&account=${account_id}&project=${project_id}`,
                 this.selects.ticket.value = "Choose (optional)";
                 this.selects.ticket.selected = 0;
@@ -252,8 +161,8 @@ ngOnInit()
                 break;
             }
             // dont change ticket on edit
-            if (!this.time.task_type_id){
-                this.selects.ticket.hidden = this.time.is_project_log || this.time.task_type_id || false;
+            if (!this.data.task_type_id){
+                this.selects.ticket.hidden = this.data.is_project_log || this.data.task_type_id || false;
                 this.selects.ticket.url = `tickets?status=open&account=${account_id}&project=${event.id}`,
                 this.selects.ticket.value = "Choose (optional)";
                 this.selects.ticket.selected = 0;
@@ -393,82 +302,22 @@ ngOnInit()
         return date ? getDateTime(date, showmonth, istime) : null;
     }
 
-    setMinTime(date) {
-        return (date || this.time.date || (new Date()).toJSON().substring(0,4));
-    }
-
-    setMaxTime(date) {
-        return (date || this.time.date || (new Date()).toJSON().substring(0,4));
-    }
-
-    getStartDate(time) {
-        return (time || this.time.date || (new Date()).toJSON().substring(0,19));
-    }
-
-    setStartDate(time){
-        if (time)
-        {
-            this.start_time = time.substring(0,19);
-            if (this.stop_time)
-            {
-               this.updateHours();
-            }
-        }
-    }
-
-    setStopDate(time){
-        if (time)
-        {
-            this.stop_time = time.substring(0,19);
-            if (this.start_time)
-            {
-                this.updateHours();
-            }
-        }
-    }
-
-    updateHours()
-    {
-        let timecount : number = this.getInterval();
-        this.timecount = timecount.toFixed(2);
-    }
-
-    endsWith(str, search)
-    {
-        str = str || "";
-        var this_len = str.length;
-        return str.substring(this_len - search.length, this_len) === search;
-    }
-
-    getInterval()
-    {
-        var start_time = this.start_time;
-        if (!this.endsWith(this.start_time, "Z"))
-            start_time += "Z"; 
-        var stop_time = this.stop_time;
-        if (!this.endsWith(this.stop_time, "Z"))
-            stop_time += "Z"; 
-        return Number(Math.round((+(new Date(stop_time)) - +(new Date(start_time))) / 60000)/60);
-    }
-
 
     getFixed(value) {
         return Number(value || "0").toFixed(2).toString();
     }
     
-    close(data?) {
-        this.view.dismiss(data);
-    }
     getCurrency(value) {
         return getCurrency(value);
     }
 
+// show invoice ?
     viewInvoice() {
         let invoice = {
-            account_name : this.time.account_name,
-            id: this.time.invoice_id,
-            account_id: this.time.account_id,
-            project_id: this.time.project_id
+            account_name : this.data.account_name,
+            id: this.data.invoice_id,
+            account_id: this.data.account_id,
+            project_id: this.data.project_id
         };
 
         this.nav.push(InvoiceDetailsPage, invoice);
