d2199a315e47bb95ee0a0f78a8f5c763a61bc9e0
Eugene <jtrue@mail.ru>
2018-12-05 14:18:26 +0200

    ed add time

--- app/components/action-button/action-button.ts
+++ app/components/action-button/action-button.ts
@@ -32,8 +32,7 @@ export class ActionButtonComponent {
         let myModal = Modal.create(page, this.data);
         myModal.onDismiss(data1 => { 
             //console.log(this.nav);
-            //console.log(this.data);
-            if (data1 && !this.data.tech && !this.data.account && data1 != "close")
+            if (data1 && !this.data.tech && !this.data.account && !data1.task_type_id)
                 this.nav.push(TicketDetailsPage, data1);
         });
         this.nav.present(myModal);
