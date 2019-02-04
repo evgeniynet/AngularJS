ec1c60324d3d7b6df764739aceeaf51cc01cbfe0
dima <0684509627@ukr.net>
2019-02-04 16:26:53 +0200

    added design of filter to todos Tkt#9936

--- app/pages/todos/todos.ts
+++ app/pages/todos/todos.ts
@@ -1,15 +1,19 @@
 import {Page, Config, Nav, NavParams, Modal} from 'ionic-angular';
+import {forwardRef} from '@angular/core';
 import {TodoCreatePage} from '../todo-create/todo-create';
 import {TodoListComponent} from '../../components/components';
+import {SelectListComponent} from '../../components/select-list/select-list';
 
 @Page({
     templateUrl: 'build/pages/todos/todos.html',
-    directives: [TodoListComponent],
+    directives: [forwardRef(() => SelectListComponent), TodoListComponent],
 })
 export class TodosPage {
 
     is_empty: boolean = false;
     params: any;
+    selects: any;
+    test: boolean;
     //undone: number = 0;
     
     constructor(private nav: Nav, private config: Config, private navParams: NavParams) {
@@ -20,6 +24,7 @@ export class TodosPage {
         this.params = this.navParams.data || {};
         //this.pager = { page: 0 };
         this.params.user = { id: this.params.user_id || this.config.current.user.user_id, name: this.params.user_name || "" };
+        this.params.tech = { id: this.params.tech_id || 0, name: this.params.tech_name || "" };
         /*let cachename = addp("todos", "assigned_id", this.params.user.id);
            this.todoProvider.todos$[cachename].subscribe(
                 data => {
@@ -28,11 +33,40 @@ export class TodosPage {
                     this.undone = count;
                 });
                 */
+
+                this.selects = {
+            "tech" : {
+                name: "Tech", 
+                value: "--All " +this.config.current.names.tech.p+" --",
+                default: "--All " +this.config.current.names.tech.p+" --",
+                isnew_disabled: true,
+                selected: (this.params.tech || {}).id || 0,
+                url: "technicians",
+                hidden: false
+            }
+        };
         }
+
+    saveSelect(event){
+        let name = event.type;
+        this.selects[name].selected = event.id;
+        this.selects[name].value = event.name;
+    }
 
     AddTodo()
     {
         let myModal = Modal.create(TodoCreatePage, {"list_id": "" });
         this.nav.present(myModal);
     }
+    toggle(){
+        this.test = !this.test;
+        if (this.test){
+            setTimeout(() => {
+        let t = document.getElementsByClassName("open-filter");
+        t = t[t.length - 1];
+        t && t.focus();
+        }, 500);
+        }
+    }
+
 }
