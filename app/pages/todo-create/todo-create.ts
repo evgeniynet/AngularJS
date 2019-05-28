8a62067d6e97e38325591bd3c3e8abf7a29bcf4e
dima <0684509627@ukr.net>
2019-05-28 15:59:16 +0300

    selects-lists on todo-create Tkt#16095

--- app/pages/todo-create/todo-create.ts
+++ app/pages/todo-create/todo-create.ts
@@ -1,9 +1,13 @@
 import {Page, Config, Nav, NavParams, ViewController} from 'ionic-angular';
+import {forwardRef} from '@angular/core';
 import {getDateTime, getPickerDateTimeFormat, htmlEscape, linebreaks} from '../../directives/helpers';
+import {SelectListComponent} from '../../components/select-list/select-list';
 import {TodoProvider} from '../../providers/todo-provider';
 
 @Page({
     templateUrl: 'build/pages/todo-create/todo-create.html',
+        directives: [forwardRef(() => SelectListComponent)],
+
 })
 export class TodoCreatePage {
 
@@ -11,6 +15,8 @@ export class TodoCreatePage {
     he: any;
     displayFormat: string;
     UserDateOffset: number = -5;
+    account_id: number;
+    selects: any = {};
 
     constructor(private nav: Nav, private navParams: NavParams, private todoProvider: TodoProvider, private config: Config, private view: ViewController) {
     this.he = config.current.user;
@@ -22,7 +28,46 @@ export class TodoCreatePage {
         this.UserDateOffset = this.config.getCurrent("timezone_offset");
         this.displayFormat = getPickerDateTimeFormat(false, false);
         this.todo.due_date = this.todo.due_date ? this.AddHours(this.todo.due_date, this.UserDateOffset) : "";
-    }
+
+        let recent : any = {};
+        if (!this.todo.account)
+        {
+                recent = this.config.current.recent || {};
+        }
+
+        this.account_id = (this.todo.account || {}).id || (recent.account || {}).selected || this.he.account_id || -1;
+        let project_id = (this.todo.project || {}).id || this.todo.project_id || (recent.project || {}).selected || 0;
+       this.selects.tech = {
+            name: "Tech", 
+            value: (this.todo.tech || {}).name || "Choose",
+            selected: (this.todo.tech || {}).id || 0,
+            url: this.config.current.is_allow_user_choose_tech && this.config.current.is_allow_user_choose_queue_only ? "users?role=queue" : "technicians",
+            hidden: false
+        };
+ 
+        this.selects.account = {
+            name: "Account", 
+            value: (this.todo.account || {}).name || (recent.account || {}).value || this.he.account_name,
+            selected: this.account_id,
+            url: "accounts?is_with_statistics=false&limit=500",
+            hidden: false
+        };
+        this.selects.project =  {
+                name: "Project", 
+                value: (recent.project || {}).value || "Default",
+                selected: project_id,
+                url: `projects?account=${this.account_id}&is_with_statistics=false`,
+                hidden: false
+            };
+        this.selects.ticket =  {
+                    name: "Ticket", 
+                    value: this.todo.ticket_number ? `#${this.todo.ticket_number}: ${this.todo.ticket_subject}` : "Choose (optional)",
+                    selected: this.todo.ticket_number || 0,
+                    url: `tickets?status=open&account=${this.account_id}&project=${project_id}`,
+                    hidden: this.todo.is_project_log || false,
+                    is_disabled: this.todo.task_type_id
+                };     
+   }
 
     AddHours(date, hours)
 {
@@ -34,6 +79,48 @@ export class TodoCreatePage {
     }
     return date;
 }
+
+    saveSelect(event){
+        let name = event.type;
+        this.account_id = this.selects.account.selected;
+        let project_id = this.selects.project.selected;
+        switch (name) {
+            case "account" :
+                this.selects.project.url = `projects?account=${event.id}&is_with_statistics=false`;
+                this.selects.project.value = "Default";
+                this.selects.project.selected = 0;
+                project_id = 0;
+                this.selects.account.value = event.name;
+                this.selects.account.selected = event.id;
+                this.account_id = event.id;
+
+                if (!this.todo.task_type_id){
+                this.selects.ticket.url = `tickets?status=open&account=${this.account_id}&project=${project_id}`,
+                this.selects.ticket.value = "Choose (optional)";
+                this.selects.ticket.selected = 0;
+            }
+                break;
+            case "project" :
+            if (this.selects.project.selected === event.id)
+            {
+                break;
+            }
+            this.selects.project.value = event.name;
+            this.selects.project.selected = event.id;
+            if (!this.todo.task_type_id){
+                this.selects.ticket.hidden = this.todo.is_project_log || this.todo.task_type_id || false;
+                this.selects.ticket.url = `tickets?status=open&account=${this.account_id}&project=${event.id}`,
+                this.selects.ticket.value = "Choose (optional)";
+                this.selects.ticket.selected = 0;
+            }
+            project_id = event.id;
+            break;    
+            default:
+                    this.selects[name].selected = event.id;
+                    this.selects[name].value = event.name || "Default";
+            break;
+        }
+    }
 
     onSubmit(form) {
         let hours = this.todo.estimated_remain || "";
@@ -57,14 +144,15 @@ export class TodoCreatePage {
             let data = {
                 task_id:null,
                 title: this.todo.title,
+                account_id: this.account_id,
+                project_id: this.selects.project.selected,
                 text: this.todo.note,
-                assigned_id: this.he.user_id,
+                assigned_id: this.selects.tech.selected,
                 estimated_remain: hours,
                 due_date: this.AddHours(this.todo.due_date, -1*this.UserDateOffset) || "",
                 notify: false,
                 list_id: this.todo.list_id,
-                ticket_key: null,
-                project_id:0
+                ticket_key: this.selects.ticket.selected,
             };
 
             this.todoProvider.addTodo(data).subscribe(
