6cb9dc62752e4995225fb7dcf4e2acc747e409b4
Eugene <jtrue@mail.ru>
2020-10-21 15:59:02 +0300

    Tkt #18766 Mobile: To Do Access

--- app/pages/todo-create/todo-create.ts
+++ app/pages/todo-create/todo-create.ts
@@ -1,4 +1,4 @@
-import {Page, Config, Nav, NavParams, ViewController} from 'ionic-angular';
+import {Page, Config, Nav, NavParams, ViewController, Alert} from 'ionic-angular';
 import {forwardRef} from '@angular/core';
 import {getDateTime, getPickerDateTimeFormat, htmlEscape, linebreaks} from '../../directives/helpers';
 import {SelectListComponent} from '../../components/select-list/select-list';
@@ -11,12 +11,14 @@ import {TodoProvider} from '../../providers/todo-provider';
 })
 export class TodoCreatePage {
 
-    todo: any;
+    todo: any = {};
     he: any;
     displayFormat: string;
     UserDateOffset: number = -5;
     account_id: number;
     selects: any = {};
+    title: string = "";
+    istaxable: boolean = true;
 
     constructor(private nav: Nav, private navParams: NavParams, private todoProvider: TodoProvider, private config: Config, private view: ViewController) {
     this.he = config.current.user;
@@ -24,11 +26,23 @@ export class TodoCreatePage {
     
     ngOnInit()
     {
+        
         this.todo = this.navParams.data || {};
         this.UserDateOffset = this.config.getCurrent("timezone_offset");
         this.displayFormat = getPickerDateTimeFormat(false, false);
         this.todo.due_date = this.todo.due_date ? this.AddHours(this.todo.due_date, this.UserDateOffset) : "";
-
+        let name = this.todo.assigned_name;
+            if (this.todo.id)
+            {
+                this.title = `Edit ToDo ${this.todo.title} by\u00a0${name}`;
+                //fix todozone
+                //this.start_todo = this.AddHours(this.todo.start_todo, this.todo.offset);
+                //this.stop_todo = this.AddHours(this.todo.stop_todo, this.todo.offset);
+            }
+            else{
+                this.title = "Add ToDo";
+                this.istaxable = true;
+            }
         let recent : any = {};
         if (!this.todo.account)
         {
@@ -39,8 +53,8 @@ export class TodoCreatePage {
         let project_id = (this.todo.project || {}).id || this.todo.project_id || (recent.project || {}).selected || 0;
        this.selects.tech = {
             name: "Tech", 
-            value: (this.todo.tech || {}).name || "Choose",
-            selected: (this.todo.tech || {}).id || 0,
+            value: this.todo.assigned_name || "Choose",
+            selected: this.todo.assigned_id || 0,
             url: this.config.current.is_allow_user_choose_tech && this.config.current.is_allow_user_choose_queue_only ? "users?role=queue" : "technicians",
             hidden: false
         };
@@ -66,7 +80,9 @@ export class TodoCreatePage {
                     url: `tickets?status=open&account=${this.account_id}&project=${project_id}`,
                     hidden: this.todo.is_project_log || false,
                     is_disabled: this.todo.task_type_id
-                };     
+                };    
+                        //console.log(this.todo);
+
    }
 
     AddHours(date, hours)
@@ -123,7 +139,7 @@ export class TodoCreatePage {
     }
 
     onSubmit(form) {
-        let hours = this.todo.estimated_remain || "";
+        let hours = (this.todo.estimated_remain + "") || "";
         if (form.valid) {
             if (hours) {
             if (~hours.indexOf('.')){
@@ -141,25 +157,36 @@ export class TodoCreatePage {
             //proof double click
             if (this.todo.in_progress && Date.now() - this.todo.in_progress < 2500) {return;}
             this.todo.in_progress = Date.now();
+            var isEdit = !!this.todo.id;
+            var note = htmlEscape((this.todo.text + "").trim()).substr(0, 5000);
+    
             let data = {
-                task_id:null,
+                task_id: this.todo.id,
                 title: this.todo.title,
                 account_id: this.account_id,
                 project_id: this.selects.project.selected,
-                text: this.todo.note,
+                text: note,
                 assigned_id: this.selects.tech.selected,
                 estimated_remain: hours,
                 due_date: this.AddHours(this.todo.due_date, -1*this.UserDateOffset) || "",
-                notify: false,
+                notify: true,
                 list_id: this.todo.list_id,
                 ticket_key: this.selects.ticket.selected,
+                //edit todo
+//time_non_working_hours: 0,
+//time_hours: 1,
+//time_no_invoice: false,
+//time_is_taxable: true,
+//time_task_type_id: 56,
             };
+
+                    //console.log(data);
 
             this.todoProvider.addTodo(data).subscribe(
                 data => {
                     this.nav.alert('Todo was successfully added :)');
-                    this.todoProvider.getTodos(this.he.user_id, "", "false", { page: 0, limit: 5000 });
-                    setTimeout(() => this.close(), 500);
+                    //this.todoProvider.getTodos(this.he.user_id, "", "false", { page: 0, limit: 250 });
+                    setTimeout(() => this.close(2), 500);
                 },
                 error => {
                     console.log(error || 'Server error');
@@ -180,11 +207,39 @@ export class TodoCreatePage {
         if (time)
         {
             this.todo.due_date = time.substring(0,19);
-        }
+        };
     }
+
+        deleteTodo(){
+            let prompt = Alert.create({
+             title: 'Do you really want to remove Todo #' + this.todo.id,
+             buttons: [
+             {
+                 text: 'Cancel',
+                 handler: data => {
+                     console.log('Cancel clicked');
+                 }
+             },
+             {
+                 text: 'Delete',
+                 handler: data => {
+                    this.todoProvider.deleteTodo(this.todo.id).subscribe(
+                    data => {
+                        //this.todoProvider.getTodos(this.he.user_id, "", "false", { page: 0, limit: 250 });
+                    setTimeout(() => this.close(this.todo.id), 500);
+                   }, 
+            error => { 
+                console.log(error || 'Server error');}
+        ); 
+                 }
+             }
+             ]
+         });
+         this.nav.present(prompt);
+        }
     
-    close() {
-        this.view.dismiss();
+    close(id?) {
+        this.view.dismiss(id);
     }
 }
 
