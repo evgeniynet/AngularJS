9d0d1fe1e185996a1568886342b1e208938a61d9
Eugene <eugene@micajah.com>
2021-01-13 15:48:52 +0200

    Tkt #20159: Update assignments always display error

--- app/providers/config.ts
+++ app/providers/config.ts
@@ -6,6 +6,6 @@ export let Site = isSD ? 'sherpadesk.com/' : 'bigwebapps.com/';
 export let MobileSite = 'https://m.' + Site;
 export let AppSite = 'https://app.' + Site;
 export let ApiSite = 'https://api.' + Site;
-export let  year="2020";
+export let  year="2021";
 export let  appVersion = "97";
 export let  AppTitle = isSD ? 'SherpaDesk ' : 'HelpDesk ';
